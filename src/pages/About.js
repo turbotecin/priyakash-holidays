13860dab9b10ed7aa5d4f5e49d6346783e665c67
asit <asit.poddar@redoq.co.uk>
2025-11-02 19:19:48 +0530

    basic files update

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+export default function About() {
+  return (
+    <Container className="py-5">
+      <h2>About Priyakash Holidays</h2>
+      <p>
+        We are a trusted travel agency from Kolkata, offering tailor-made tour packages across India and abroad.
+      </p>
+    </Container>
+  );
+}
